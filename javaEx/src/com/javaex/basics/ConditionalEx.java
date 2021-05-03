988d89b8df2c6e004d53deb244baa26069c2b5e9
namgung <stomer1@naver.com>
2021-05-03 14:53:03 +0900

    조건문 if else

---
+++ javaEx/src/com/javaex/basics/ConditionalEx.java
@@ -0,0 +1,71 @@
+package com.javaex.basics;
+
+import java.util.Scanner;
+
+public class ConditionalEx {
+
+	public static void main(String[] args) {
+//		ifElseEx();
+		ifElseEx2();
+	}
+	private static void ifElseEx2() {
+		//정수 입력
+		//양수일 때
+		//	짝수, 홀수
+		//음수일 때 -> 음수
+		//0이면 0
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("정수를 입력하세요:");
+		
+		int num = scanner.nextInt();
+		
+		if (num > 0) {
+			if(num %2 == 0) {
+				System.out.println("짝수");
+			}else {
+				System.out.println("홀수");
+			}
+		}else {
+			if (num == 0) {
+				System.out.println("0입니다.");
+			}else {
+				System.out.println("음수입니다");
+			}
+		}
+		
+		scanner.close();
+			
+		
+	}
+	private static void ifElseEx() {
+		//정수를 입력 받아서
+		// 양수, 음수, 0을 출력
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("정수를 입력하세요:");
+		
+		int num = scanner.nextInt();
+		/*
+		if (num > 0) {
+			System.out.println("양수입니다.");
+			
+		}else if (num < 0) {
+			System.out.println("음수입니다");
+		}else {
+			System.out.println("0입니다.");
+			
+		}
+		*/
+		//중첩 if
+		if(num == 0) {
+			System.out.println("0입니다");
+		}else {
+			if(num > 0) {
+				System.out.println("양수입니다");
+			}else {
+				System.out.println("음수입니다");
+			}
+		}
+		scanner.close();
+	}
+
+}
