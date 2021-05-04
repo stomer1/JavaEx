20549f93b971088dfc6eb0b1fb4e9d23551a80c7
Namgung <stomer1@naver.com>
2021-05-04 17:54:54 +0900

    연습 문제

--- javaEx/src/com/javaex/basics/practice.java
+++ javaEx/src/com/javaex/basics/practice.java
@@ -3,11 +3,51 @@ package com.javaex.basics;
 public class practice {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 1; i++) {
-			System.out.print("* ");
+		arrayCopySystem();
+	
+		//problem01
+		int[] data = {1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31};
+		int total = 0;
+		int num = 0;
+		for (int i = 0; i < data.length;  i++) {
+			System.out.println(data[i]);
+			if(data[i] % 3 == 0) {	
+				num++;
+				System.out.println("3배수의 개수:" +num);
+				total += data[i];
+				
+				
+			}
 			
+			System.out.println("3배수의 합: " + total);
 		}
-
+		//problem02
+		int total1 = 0;
+		float avg = 0;
+		
+		int data2[] = {10, 20, 30, 40, 50};
+		for (int i = 0; i < data2.length; i++) {
+			total1 += data2[i];
+			avg = (total1/5);
+		}
+		System.out.println("총합: " + total1);
+		System.out.println("평균: " +avg);
+	}
+		//problem03
+		private static void arrayCopySystem() {
+		char c[] = {'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ',  'p', 'e', 'n', 'c', 'i', 'l' };
+		char target[] = new char[16];
+		
+		System.arraycopy(c, 0, target, 0, c.length);
+		
+		for (int i = 0; i < target.length; i++) {
+			System.out.print(target[i]);
+		}
+		
+		}
+		
+	
+		
 	}
 
-}
+
