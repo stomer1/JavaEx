073283ed3eb5710a7592ab94e1e90a9d6555b794
Namgung <stomer1@naver.com>
2021-05-05 23:55:21 +0900

    연습문제

--- javaEx/src/com/javaex/basics/practice.java
+++ javaEx/src/com/javaex/basics/practice.java
@@ -4,7 +4,7 @@ public class practice {
 
 	public static void main(String[] args) {
 		arrayCopySystem();
-	
+		multiDimArray();
 		//problem01
 		int[] data = {1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31};
 		int total = 0;
@@ -32,10 +32,18 @@ public class practice {
 		}
 		System.out.println("총합: " + total1);
 		System.out.println("평균: " +avg);
+		
+		//problem04
+		
+		
+		for (int i = 0; i <= 5; i++) {
+			int random = (int)(Math.random()*45)+1;
+			System.out.print(random + " ");
+			}
 	}
 		//problem03
 		private static void arrayCopySystem() {
-		char c[] = {'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ',  'p', 'e', 'n', 'c', 'i', 'l' };
+		char c[] = {'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ',  'p', 'e', 'n', 'c', 'i', 'l'};
 		char target[] = new char[16];
 		
 		System.arraycopy(c, 0, target, 0, c.length);
@@ -43,11 +51,43 @@ public class practice {
 		for (int i = 0; i < target.length; i++) {
 			System.out.print(target[i]);
 		}
+		System.out.println();
+		char c1[] = {'T', 'h', 'i', 's', ',', 'i', 's', ',', 'a', ',',  'p', 'e', 'n', 'c', 'i', 'l'};
+		char target1[] = new char[16];
 		
+		System.arraycopy(c1, 0, target1, 0, c1.length);
+		
+		for (int i = 0; i < target1.length; i++) {
+			System.out.print(target1[i]);
+		}
+		System.out.println();
+		}
+		private static void multiDimArray() {
+			int[][] twoDimen = new int[5][5];
+			
+			int scoreboard[][] = {
+					{80, 75, 90, 95, 78 },
+					{92, 88, 89, 92, 70 },
+					{78, 80, 85, 86, 63 },
+					{83, 84, 89, 87, 75 },
+					{89, 83, 93, 94, 78 }
+			};
+			int total3 = 0;
+			
+			for(int row = 0; row < scoreboard.length; row++) {
+				for(int col = 0; col < scoreboard[row].length; col++) {
+					System.out.print(scoreboard[row][col] + "\t");
+					
+					if(scoreboard[row][col] >= 85) {
+						System.out.print(scoreboard[row][col]);
+					}
+					
+				}
+				System.out.println();
+			}
 		}
 		
-	
 		
 	}
-
+	
 
