ba624db84469a3a09e3c85165f7c57f8e6515a7a
Namgung <stomer1@naver.com>
2021-05-10 10:41:15 +0900

    정렬과 검색

--- javaEx/src/com/javaex/api/arrayclass/ArraysClassEx.java
+++ javaEx/src/com/javaex/api/arrayclass/ArraysClassEx.java
@@ -7,7 +7,51 @@ public class ArraysClassEx {
 	public static void main(String[] args) {
 //		printArrayEx();
 //		copyArrayEx();
-		sortEx();
+//		sortEx();
+//		sortCustomEx();
+		searchEx();
+	}
+//	배열 내 검색
+	private static void searchEx() {
+		Integer num2[] = { 5, 6, 3, 2, 4, 1, 7, 9, 8, 10 };
+		// 자바는 기본적으로 이진 검색
+		// 이진 검색은 검색 이전에 먼저 배열이 정렬되어 있어야 한다
+		Arrays.sort(num2);
+		System.out.println("정렬된 원본: " + Arrays.toString(num2));
+		int index = Arrays.binarySearch(num2, 8);
+		System.out.println("8의 인댁스:" + index);
+		
+		// String 배열의 검색
+		String[] str = {"Java", "C", "C++", "Python", "Linux"};
+		Arrays.sort(str); // 인댁스 검색 이전에 정렬 우선
+		System.out.println("원본: " +  Arrays.toString(str));
+		index = Arrays.binarySearch(str, "Python");
+		System.out.println("Python의 인댁스 : " + index);
+		
+		// 사용자 정의 객체의 검색
+		Member[] members = {
+				new Member("홍길동"),
+				new Member("고길동"),
+				new Member("장길산")
+		};
+		Arrays.sort(members);
+		System.out.println("원본: " +  Arrays.toString(members));
+		index = Arrays.binarySearch(members, new Member("홍길동"));
+		System.out.println("홍길동의 인댁스 : " + index);
+		
+	}
+	// 정렬(사용자 정의 클래스)
+	private static void sortCustomEx() {
+		Member[] members = {
+				new Member("홍길동"),
+				new Member("고길동"),
+				new Member("장길산")
+		};
+		System.out.println("원본: " +  Arrays.toString(members));
+		Arrays.sort(members);
+		System.out.println("정렬(오름차순): " +  Arrays.toString(members));
+		Arrays.sort(members, Collections.reverseOrder());
+		System.out.println("정렬(내림차순): " +  Arrays.toString(members));
 	}
 	
 	//	정렬
@@ -20,6 +64,13 @@ public class ArraysClassEx {
 		Arrays.sort(nums);
 		System.out.println("정렬 후(오름차순):" + Arrays.toString(nums));
 		
+		Integer num2[] = { 5, 6, 3, 2, 4, 1, 7, 9, 8, 10 };
+		System.out.println("원본:" + Arrays.toString(num2));
+		
+		// 정렬(내림차순)
+		Arrays.sort(num2,Collections.reverseOrder());
+		System.out.println("정렬 후(내림차순) : " + Arrays.toString(num2));
+		
 //		nums = new int[] { 5, 6, 3, 2, 4, 1, 7, 9, 8, 10 }; 
 //		System.out.println("원본:" + Arrays.toString(nums));
 //		
@@ -28,10 +79,7 @@ public class ArraysClassEx {
 		
 	}
 	
-	//	배열 내 검색
-	private static void searchEx() {
-		
-	}
+	
 	
 	private static void copyArrayEx() {
 		int arr[] = new int[] {
