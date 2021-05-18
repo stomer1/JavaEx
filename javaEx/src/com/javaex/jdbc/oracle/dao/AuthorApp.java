5da555bd81213790429d0a0cc9a273dba2a88155
purpleapples <tjdvkf19@gmail.com>
2021-05-18 10:52:05 +0900

    DAO Pattern : Select

---
+++ javaEx/src/com/javaex/jdbc/oracle/dao/AuthorApp.java
@@ -0,0 +1,36 @@
+package com.javaex.jdbc.oracle.dao;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		selectAll();
+		insertAuthor();
+	}
+	private static void insertAuthor() {
+		// Scanner에서 이름 입력 -> INSERT
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+	private static void selectAll() {
+		AuthorDAO dao = new AuthorDAOimpl();
+		List<AuthorVO> list = dao.getList();
+		
+		// Iterator
+		Iterator<AuthorVO> it = list.iterator();
+		
+		System.out.println("========== Author List");
+		
+		while(it.hasNext()) {
+			AuthorVO item = it.next(); // iterator에서 요소 추출
+			System.out.printf("%d\t%s\t%s%n",
+					item.getId(),
+					item.getAuthorName(),
+					item.getAuthorDesc());
+		}
+	}
+
+}
